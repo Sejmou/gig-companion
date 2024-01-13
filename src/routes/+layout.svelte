25b35f3c11172708f33e919e07228dc04cb0483a
Sejmou <samo.kolter@gmail.com>
2024-01-13 15:21:11 +0100

    remove breadcrumbs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,29 +24,7 @@
 
 <div class="flex flex-col min-h-[100vh] bg-base-200">
 	<Navbar />
-	<div class="w-full px-4 max-w-screen-lg mx-auto">
-		<div class="text-sm breadcrumbs">
-			{#if pathComponents.length > 0}
-				<ul>
-					<li>
-						<a href="/"><iconify-icon icon="mdi:home"></iconify-icon></a>
-					</li>
-					{#each pathComponents as component, i}
-						<li>
-							{#if i < pathComponents.length - 1}
-								<a href={'/' + pathComponents.slice(0, i + 1).join('/')}
-									>{prettifyComponentName(component)}</a
-								>
-							{:else}
-								{prettifyComponentName(component)}
-							{/if}
-						</li>
-					{/each}
-				</ul>
-			{/if}
-		</div>
-	</div>
-	<main class="flex-1 flex flex-col p-4 pt-0 w-full max-w-screen-lg mx-auto my-auto box-border">
+	<main class="flex-1 flex flex-col p-4 w-full max-w-screen-lg mx-auto my-auto box-border">
 		<slot />
 	</main>
 	<Footer />
